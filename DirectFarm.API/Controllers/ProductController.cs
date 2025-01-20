50bab617ede6e8cd1a35f49d93f87fb265a2d9eb
abiybez <abiybata@gmail.com>
2025-01-20 13:37:39 +0300

    Delete to Deleted Method

--- DirectFarm.API/Controllers/ProductController.cs
+++ DirectFarm.API/Controllers/ProductController.cs
@@ -45,7 +45,7 @@ namespace DirectFarm.API.Controllers
             var result = await this.mediator.Send(new GetProductImageQuery(img.Id));
             return result;
         }
-        [HttpPost("DeleteProduct")]
+        [HttpDelete("DeleteProduct")]
         public async Task<Response<bool>> Delete(BaseModel model)
         {
             var result = await this.mediator.Send(new DeleteProductCommand(model.Id));
